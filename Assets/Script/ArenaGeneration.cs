5d774ba4a4578e290e21a25a82468c6ab422e3d8
MrChuckchucky <b.devienne@rubika-edu.com>
2015-12-17 17:07:26 +0100

    Création du tableau des murs

--- Assets/Script/ArenaGeneration.cs
+++ Assets/Script/ArenaGeneration.cs
@@ -12,9 +12,9 @@ public class ArenaGeneration : MonoBehaviour
     public int height;
     public int width;
     public int chanceSpawnWall;
-
+    
     private GameObject[,] arena;
-    private GameObject[,] wallArena;
+    private int[,][] wallArena;
     private float wallHeight;
     private float floorSide;
     private int randomSpawnWall;
@@ -22,19 +22,18 @@ public class ArenaGeneration : MonoBehaviour
 	private GameObject papaSol;
 	private GameObject papaMur;
 
-    static ArenaGeneration instance;
-
-    public static ArenaGeneration Instance
-    {
-        get
-        {
-            return instance;
-        }
-    }
+    public static ArenaGeneration instance = null;
 
     void Awake()
     {
-        instance = this;
+        if(ArenaGeneration.instance == null)
+        {
+            instance = this;
+        }
+        else if(ArenaGeneration.instance != this)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     // Use this for initialization
@@ -51,11 +50,20 @@ public class ArenaGeneration : MonoBehaviour
         wallHeight = wall.transform.localScale.y / 2;
         floorSide = floor.transform.localScale.x / 2;
         arena = new GameObject[height, width];
-        wallArena = new GameObject[height, width];
-        for(int i = 0; i < height; i++)
+        wallArena = new int[height, width][];
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                // Création du tableau des murs dans la case [i, j]
+                wallArena[i, j] = new int[4];
+            }
+        }
+        for (int i = 0; i < height; i++)
         {
             for(int j = 0; j < width; j++)
             {
+                // Création du sol de l'arène et envoi dans la fonction de création des murs random
                 WallCreation(i, j);
                 GameObject currentFloor = Instantiate(floor, new Vector3(i, 0, j), Quaternion.identity) as GameObject;
                 CubeScript CS = currentFloor.GetComponent<CubeScript>();
@@ -63,7 +71,6 @@ public class ArenaGeneration : MonoBehaviour
                 listCubeScript.Add(CS);
                 arena[i, j] = currentFloor;
                 currentFloor.transform.parent = papaSol.transform;
-
             }
         }
 
@@ -75,57 +82,104 @@ public class ArenaGeneration : MonoBehaviour
 
     void WallCreation(int a, int b)
     {
+        // Création des murs autour de l'arène
         if (a == 0)
         {
+            // left
+            wallArena[a, b][2] = 1;
             GameObject currentWallContour = Instantiate(wall, new Vector3(a - floorSide, wallHeight, b), Quaternion.Euler(0, 90, 0)) as GameObject;
 			currentWallContour.transform.parent = papaMur.transform;
         }
         if (a == height - 1)
         {
+            // right
+            wallArena[a, b][1] = 1;
             GameObject currentWallContour = Instantiate(wall, new Vector3(a + floorSide, wallHeight, b), Quaternion.Euler(0, 90, 0)) as GameObject;
 			currentWallContour.transform.parent = papaMur.transform;
         }
         if (b == 0)
         {
+            // bottom
+            wallArena[a, b][3] = 1;
             GameObject currentWallContour = Instantiate(wall, new Vector3(a, wallHeight, b - floorSide), Quaternion.identity) as GameObject;
 			currentWallContour.transform.parent = papaMur.transform;
         }
         if (b == width - 1)
         {
+            // top
+            wallArena[a, b][0] = 1;
             GameObject currentWallContour = Instantiate(wall, new Vector3(a, wallHeight, b + floorSide), Quaternion.identity) as GameObject;
 			currentWallContour.transform.parent = papaMur.transform;
         }
 		
 		
-		
+		// Création des murs au milieu de l'arène
         randomSpawnWall = Random.Range(0, 100);
         if(randomSpawnWall < chanceSpawnWall)
         {
             GameObject currentWall = null;
 
             wallOrientation = Random.Range(0, 4);
-            switch(wallOrientation)
+            switch (wallOrientation)
             {
                 case 0:
+                    // bottom
+                    try
+                    {
+                        // La case du dessous récupère le mur au dessus
+                        wallArena[a, b - 1][0] = 1;
+                    }
+                    catch
+                    {
+                        // La cellule [a, b - 1] n'existe pas
+                    }
+                    wallArena[a, b][3] = 1;
                     currentWall = Instantiate(wall, new Vector3(a, wallHeight, b - floorSide), Quaternion.Euler(0, 0, 0)) as GameObject;
                     break;
                 case 1:
+                    // left
+                    try
+                    {
+                        // La case à gauche récupère le mur à sa droite
+                        wallArena[a - 1, b][1] = 1;
+                    }
+                    catch
+                    {
+                        // La cellule [a - 1, b] n'existe pas
+                    }
+                    wallArena[a, b][2] = 1;
                     currentWall = Instantiate(wall, new Vector3(a - floorSide, wallHeight, b), Quaternion.Euler(0, 90, 0)) as GameObject;
                     break;
                 case 2:
+                    // top
+                    try
+                    {
+                        // La case du dessous récupère le mur au dessous
+                        wallArena[a, b + 1][3] = 1;
+                    }
+                    catch
+                    {
+                        // La cellule [a, b + 1] n'existe pas
+                    }
+                    wallArena[a, b][0] = 1;
                     currentWall = Instantiate(wall, new Vector3(a, wallHeight, b + floorSide), Quaternion.Euler(0, 180, 0)) as GameObject;
                     break;
                 case 3:
+                    // right
+                    try
+                    {
+                        // La case de droite récupère le mur à sa gauche
+                        wallArena[a + 1, b][2] = 1;
+                    }
+                    catch
+                    {
+                        // La cellule [a + 1, b] n'existe pas
+                    }
+                    wallArena[a, b][1] = 1;
                     currentWall = Instantiate(wall, new Vector3(a + floorSide, wallHeight, b), Quaternion.Euler(0, 270, 0)) as GameObject;
                     break;
             }
-
-			currentWall.transform.parent = papaMur.transform;
-            wallArena[a, b] = currentWall;
-        }
-        else
-        {
-            wallArena[a, b] = null;
+            currentWall.transform.parent = papaMur.transform;
         }
     }
 
@@ -133,10 +187,13 @@ public class ArenaGeneration : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
+            // Commande de Debug pour tester la génération de l'arène
             Application.LoadLevel(Application.loadedLevel);
         }
     }
 
+
+    // Ce n'est pas moi qui ait écrit cette fonction, du moins je ne me souviens pas
     public GameObject getTile(int x, int y)
     {
         if (x < height-1 && y < width-1 && x>=0 && y>=0)
